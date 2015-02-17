c0c51a07ce186064c4e1540488fdb391d878e9fb
andrevyakin <andrevyakin@gmail.com>
2015-02-17 16:59:06 +0300

    Поиск и сохранение директории в массив

--- File_Manager/File_Manager/File_Manager.cpp
+++ File_Manager/File_Manager/File_Manager.cpp
@@ -5,16 +5,49 @@
 //4. Сделать выбор диска при нажатии клавиши, напимер F2, для чего узнать какие диски есть на компе.
 
 #undef UNICODE			//Отключает UNICODE, заданный по умолчанию в настройках VS
-//это нужно для корректной работы функции GetCurrentDirectory
+						//это нужно для корректной работы функции GetCurrentDirectory
 #include <iostream>
 #include <iomanip>		//для setw() и setfill()
 #include <Windows.h>	//для корректного получения и вывода русского языка (SetConsoleOutputCP() и SetConsoleCP()), а также изменения цвета в консоле
 #include <io.h>			//для поиска файлов
 #include <conio.h>		//для _getch()
-
 using namespace std;
 
 void main()
 {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	
+	//Создать массив элементов структуры _finddata_t для хранения данных текущей дирректории
+	_finddata_t* dir = new _finddata_t[2];
+	_finddata_t* temp;
+	int count = 0;
 
+	//Получить путь к текущей дирректории
+	char path [MAX_PATH];
+	GetCurrentDirectory(sizeof(path), path);
+	strcat_s(path, "\\*.*");
+	
+	//Получить и сохранить сведения о первом экземпляре
+	int firstID = _findfirst(path, &dir[0]);
+
+	// Поиск и сохранение в массив
+	while (_findnext(firstID, &dir[++count]) != -1)
+	{
+		temp = new _finddata_t[count + 1];			//Выделяю нужную память под временныый массив
+		for (int i = 0; i < count + 1; i++)
+			temp[i] = dir[i];						//Копирую основной массив во временный
+		delete[] dir;								//Удаляю основной массив
+		dir = new _finddata_t[count + 2];			//Выделяю нужную память под основной массив
+		for (int i = 0; i < count +1 ; i++)
+			dir[i] = temp[i];						//Копирую элементы
+		delete[]temp;								//Удаляю уже не нужный временный массив
+	}
+	_findclose(firstID);
+	
+	//Для самопроверки
+	for (int i = 0; i < count; i++)
+		cout << dir[i].name << endl;
+
+	delete[]dir;
 }
